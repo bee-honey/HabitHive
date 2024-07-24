92b0000f2c85dcd7a4daab3c2b9ee7f9a7d8be3b
bee-honey <honeybeeprogrammer@gmail.com>
2024-07-24 10:32:30 -0700

    WOrking version with issues in background color

---
+++ HabitHive/Views/AppBackgroundModifier.swift
@@ -0,0 +1,18 @@
+//
+//  AppBackgroundModifier.swift
+//  HabitHive
+//
+//  Created by Naveen Keerthy on 7/24/24.
+//
+
+import SwiftUI
+
+struct AppBackgroundModifier: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AppBackgroundModifier()
+}
